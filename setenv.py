ce4a92da27a84daacf2c79fa69b04ee53d51d814
yuki-muta <85030762+yuki-muta@users.noreply.github.com>
2021-08-18 00:43:50 +0900

    Delete setenv.py

--- setenv.py
+++
@@ -1,4 +0,0 @@
-AmazonRootCA1 = "/home/pi/AWS_IoT/cert/AmazonRootCA1.pem"
-private = "/home/pi/AWS_IoT/cert/75abc540766143eea8289d34ef94fb45b2c205f01e9d5afd5da0314cb7849293-private.pem.key"
-certificate = "/home/pi/AWS_IoT/cert/75abc540766143eea8289d34ef94fb45b2c205f01e9d5afd5da0314cb7849293-certificate.pem.crt"
-n_remo_token = 'meM07t_8GTryVeTXZ5MGoiBTRcz3jkzDcn-mmtLoFRg.FM4mbn7o69_kQvjV9Q3aDrYOh96neYiktukhnX6rG7o'
